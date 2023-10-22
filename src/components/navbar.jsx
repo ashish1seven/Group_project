bf984240743614b9d6477637570706cd5fac7f76
Ashish B <cameron.ashish@gmail.com>
2023-10-23 00:13:28 +0530

    modal

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
+import Modal from "./modal";
 
 const Navbar = () => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [show, setShow] = useState(false);
   
   const toggleInput = () => {
     setIsExpanded(!isExpanded);
   };
   return (
     <div>
-      <nav className="flex items-center justify-between w-screen h-16 px-10 text-white bg-slate-900">
+      <nav className="fixed flex items-center justify-between w-screen h-16 px-10 text-white bg-slate-900">
         <a
-          className="px-3 py-1 text-2xl font-bold text-orange-400 duration-500 rounded hover:scale-125"
+          className="px-3 py-1 text-2xl font-bold text-orange-400 duration-500 rounded hover:scale-125 font-['Monteserrat',sans-serif]"
           href=""
         >
-          Name
+          Meet Junction
         </a>
         <div className="flex ">
           <button
@@ -32,7 +35,6 @@ const Navbar = () => {
               onBlur={toggleInput}
             />
           )}
-          {/* <input className="rounded bg-slate-800 text-slate-400" type="text" /> */}
         </div>
 
         <div className="flex items-center">
@@ -48,14 +50,25 @@ const Navbar = () => {
           >
             Create Events
           </a>
-          <button className="px-5 py-1 mx-2 font-medium duration-500 border rounded text-slate-400 border-slate-500 hover:scale-110">
+          <button
+            onClick={() => {
+              setShow(!show);
+            }}
+            className="px-5 py-1 mx-2 font-medium duration-500 border rounded text-slate-400 border-slate-500 hover:scale-110"
+          >
             Log In
           </button>
-          <button className="px-5 py-1 mx-2 font-medium duration-500 border rounded bg-slate-700 text-slate-100 border-slate-500 hover:scale-110">
+          <button
+            onClick={() => {
+              setShow(!show);
+            }}
+            className="px-5 py-1 mx-2 font-medium duration-500 border rounded bg-slate-700 text-slate-100 border-slate-500 hover:scale-110"
+          >
             Sign Up
           </button>
         </div>
       </nav>
+      <Modal show={show} setShow={setShow} />
     </div>
   );
 };

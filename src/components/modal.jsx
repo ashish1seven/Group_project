496b7e3457ed0710e384a7c4a265114541df2041
Ashish B <cameron.ashish@gmail.com>
2023-10-23 00:22:40 +0530

    modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -6,9 +6,25 @@ import React from "react";
 function Modal({ show, setShow }) {
   const visible = show ? "block" : "hidden";
   return (
-    <div className={"w-screen h-screen bg-[#ffffff2a] absolute top-0 left-0 font-['Monteserrat',sans-serif] "+ visible}>
+    <div
+      className={
+        "w-screen h-screen bg-[#ffffff2a] absolute top-0 left-0 font-['Monteserrat',sans-serif] " +
+        visible
+      }
+    >
+      <h1
+        className="absolute text-[12px] text-white right-[290px] top-[77px] cursor-pointer p-0"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        X
+      </h1>
       <div className="bg-white w-[780px] h-[480px] rounded-xl m-auto left-0 right-0 top-20 flex absolute shadow-2xl">
-        <div className="absolute left-0 w-1/2 h-full bg-white" id="signincontainer">
+        <div
+          className="absolute left-0 w-1/2 h-full bg-white"
+          id="signincontainer"
+        >
           <form className="flex flex-col items-center justify-center h-full px-10 text-center">
             <h1 className="text-3xl font-bold">Sign in</h1>
             <div className="flex my-5">
@@ -50,30 +66,45 @@ function Modal({ show, setShow }) {
             </button>
           </form>
         </div>
-        <div className="absolute right-0 z-10 w-1/2 h-full bg-orange-400" id="overlaysignin">
+        <div
+          className="absolute right-0 z-10 w-1/2 h-full bg-orange-400"
+          id="overlaysignin"
+        >
           <div className="flex flex-col items-center justify-center h-full text-center px-14">
             <h1 className="text-3xl font-bold ">Hello, Friend!</h1>
             <p className="my-3 text-base">
               Enter your personal details and start journey with us
             </p>
-            <button className="px-12 py-3 mt-2 border rounded-3xl border-slate-800" id="signup">
+            <button
+              className="px-12 py-3 mt-2 border rounded-3xl border-slate-800"
+              id="signup"
+            >
               Sign Up
             </button>
           </div>
         </div>
 
-        <div className="absolute left-0 z-10 w-1/2 h-full bg-orange-400" id="overlaysignup">
-        <div className="flex flex-col items-center justify-center h-full text-center px-14">
+        <div
+          className="absolute left-0 z-10 w-1/2 h-full bg-orange-400"
+          id="overlaysignup"
+        >
+          <div className="flex flex-col items-center justify-center h-full text-center px-14">
             <h1 className="text-3xl font-bold ">Welcome Back!</h1>
             <p className="my-3 text-base">
               To keep connected with us please login with your personal info
             </p>
-            <button className="px-12 py-3 mt-2 border rounded-3xl border-slate-800" id="signin">
+            <button
+              className="px-12 py-3 mt-2 border rounded-3xl border-slate-800"
+              id="signin"
+            >
               Sign In
             </button>
           </div>
-          </div>
-        <div className="absolute right-0 w-1/2 h-full bg-white" id="signupcontainer">
+        </div>
+        <div
+          className="absolute right-0 w-1/2 h-full bg-white"
+          id="signupcontainer"
+        >
           <form className="flex flex-col items-center justify-center h-full px-10 text-center">
             <h1 className="text-3xl font-bold">Create Account</h1>
             <div className="flex my-5">

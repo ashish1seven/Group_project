04bf3ecbf1041b7ed1c81ae6cc57c9307faa635b
Ashish B <cameron.ashish@gmail.com>
2023-10-25 23:46:15 +0530

    link

--- src/pages/createevents.jsx
+++ src/pages/createevents.jsx
@@ -5,7 +5,7 @@ const Createevents = () => {
   return (
     <div>
       <Link to={`create/`}>
-        <h1 className="text-5xl text-white">Create Events</h1>
+        <h1 className="text-5xl">Create Events</h1>
       </Link>
     </div>
   );

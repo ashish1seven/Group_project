525226b0ffb9aa984a3f61341455537d4d46afcc
Ashish B <cameron.ashish@gmail.com>
2023-10-27 15:58:31 +0530

    find

--- src/pages/findevents.jsx
+++ src/pages/findevents.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 
-import Navbar from "../components/navbar";
+
 
 const Findevents = () => {
   return (
     <>
-      <div>
-        <Navbar />
-          </div>
-          <div><h1 className="text-5xl relative">Find Events</h1></div>
-      
+      <div className="flex flex-col items-center justify-center p-24 ">
+        <h1 className="pb-10 text-6xl text-center">Discover</h1>
+        <div className="rounded-3xl overflow-hidden border-black border">
+          <input type="text" placeholder="Search" className="h-12 pl-3 w-80 " />
+          <select name="" id="" className="h-12  w-80 pl-3 mr-3">
+            <option disabled className="font-medium">
+              All Categories
+            </option>
+            <option value="">Dancing</option>
+            <option value="">Playing</option>
+          </select>
+        </div>
+        <div className="rounded-3xl overflow-hidden border-black border mt-4">
+          <input type="text" placeholder="Location or ZIP-code" className="h-12 pl-3 w-80 " />
+          <select name="" id="" className="h-12  w-80 pl-3 mr-3">
+            <option className="font-medium">Within any distance</option>
+            <option className="font-medium">Within 10 miles</option>
+            <option className="font-medium">Within 25 miles</option>
+            <option className="font-medium">Within 50 miles</option>
+            <option className="font-medium">Within 100 miles</option>
+          </select>
+        </div>
+        <button type="button" className="px-10 py-2 border mt-4 rounded-3xl font-semibold border-black">SEARCH</button>
+      </div>
     </>
   );
 };

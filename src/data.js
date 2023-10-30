32b3d212d4675b64742ab04fbcca198bac27ddbd
Ashish B <cameron.ashish@gmail.com>
2023-10-30 20:31:48 +0530

    added data

--- src/data.js
+++ src/data.js
@@ -38,7 +38,7 @@ export const searchData = [
         title: "bilaspur",
         cardSet: [
             {
-                id: 1,
+                id: 7,
                 image: "https://img.freepik.com/free-vector/stand-up-comedy-banner-with-red-curtains-background_1308-80995.jpg?size=626&ext=jpg&ga=GA1.2.386372595.1697587200&semt=ais",
                 date: "12/11/2023",
                 title: "stand up by akash ",
@@ -47,7 +47,7 @@ export const searchData = [
                 description: "This is a stand up comedy",
             },
             {
-                id: 2,
+                id: 8,
                 image: "https://img.freepik.com/free-vector/stand-up-comedy-banner-with-red-curtains-background_1308-80995.jpg?size=626&ext=jpg&ga=GA1.2.386372595.1697587200&semt=ais",
                 date: "23/11/2023",
                 title: "Football",
@@ -56,7 +56,7 @@ export const searchData = [
                 description: "This is a football game.",
             },
             {
-                id: 3,
+                id: 9,
                 image: "https://img.freepik.com/free-vector/stand-up-comedy-banner-with-red-curtains-background_1308-80995.jpg?size=626&ext=jpg&ga=GA1.2.386372595.1697587200&semt=ais",
                 date: "12/12/2023",
                 title: "Cocktail",
@@ -71,7 +71,7 @@ export const searchData = [
         title: "goa",
         cardSet: [
             {
-                id: 1,
+                id: 10,
                 image: "https://img.freepik.com/free-vector/stand-up-comedy-banner-with-red-curtains-background_1308-80995.jpg?size=626&ext=jpg&ga=GA1.2.386372595.1697587200&semt=ais",
                 date: "12/11/2023",
                 title: "stand up by akash ",
@@ -80,7 +80,7 @@ export const searchData = [
                 description: "This is a stand up comedy",
             },
             {
-                id: 2,
+                id: 11,
                 image: "https://img.freepik.com/free-vector/stand-up-comedy-banner-with-red-curtains-background_1308-80995.jpg?size=626&ext=jpg&ga=GA1.2.386372595.1697587200&semt=ais",
                 date: "23/11/2023",
                 title: "Football",
@@ -89,7 +89,7 @@ export const searchData = [
                 description: "This is a football game.",
             },
             {
-                id: 3,
+                id: 12,
                 image: "https://img.freepik.com/free-vector/stand-up-comedy-banner-with-red-curtains-background_1308-80995.jpg?size=626&ext=jpg&ga=GA1.2.386372595.1697587200&semt=ais",
                 date: "12/12/2023",
                 title: "Cocktail",
@@ -97,6 +97,231 @@ export const searchData = [
                 organizer: "Cocktail Cafe",
                 description: "free cocktails",
             },
+        ]
+    },
+    {
+        id: "mumbai456",
+        title: "mumbai",
+        cardSet: [
+            {
+                id: 4,
+                image: "https://example.com/mumbai-event1.jpg",
+                date: "11/15/2023",
+                title: "Bollywood Night",
+                venue: "Juhu Beach",
+                organizer: "Star Entertainment",
+                description: "A night of Bollywood music and dance."
+            },
+            {
+                id: 5,
+                image: "https://example.com/mumbai-event2.jpg",
+                date: "11/25/2023",
+                title: "Food Festival",
+                venue: "Gateway of India",
+                organizer: "Mumbai Foodies Club",
+                description: "Sample the best of Mumbai's street food."
+            },
+            {
+                id: 6,
+                image: "https://example.com/mumbai-event3.jpg",
+                date: "12/05/2023",
+                title: "Art Exhibition",
+                venue: "Mumbai Art Gallery",
+                organizer: "Creative Minds",
+                description: "Explore contemporary art by local artists."
+            }
+        ]
+    },
+{
+        id: "chennai303",
+        title: "chennai",
+        cardSet: [
+            {
+                id: 16,
+                image: "https://example.com/chennai-event1.jpg",
+                date: "11/20/2023",
+                title: "Carnatic Music Concert",
+                venue: "Music Academy",
+                organizer: "Chennai Music Society",
+                description: "Enjoy classical Carnatic music."
+            },
+            {
+                id: 17,
+                image: "https://example.com/chennai-event2.jpg",
+                date: "12/02/2023",
+                title: "Marina Beach Cleanup",
+                venue: "Marina Beach",
+                organizer: "Eco Warriors Chennai",
+                description: "Contribute to a cleaner Marina Beach."
+            },
+            {
+                id: 18,
+                image: "https://example.com/chennai-event3.jpg",
+                date: "12/10/2023",
+                title: "South Indian Food Festival",
+                venue: "Chennai Trade Center",
+                organizer: "Foodies of Chennai",
+                description: "Savor the best of South Indian cuisine."
+            }
+        ]
+    },
+{
+        id: "kolkata404",
+        title: "kolkata",
+        cardSet: [
+            {
+                id: 19,
+                image: "https://example.com/kolkata-event1.jpg",
+                date: "11/25/2023",
+                title: "Durga Puja Festival",
+                venue: "Various Locations",
+                organizer: "Kolkata Cultural Committee",
+                description: "Experience the grandeur of Durga Puja."
+            },
+            {
+                id: 20,
+                image: "https://example.com/kolkata-event2.jpg",
+                date: "12/05/2023",
+                title: "Kolkata Book Fair",
+                venue: "Salt Lake Central Park",
+                organizer: "Kolkata Literary Society",
+                description: "Explore a world of books and literature."
+            },
+            {
+                id: 21,
+                image: "https://example.com/kolkata-event3.jpg",
+                date: "12/15/2023",
+                title: "Rabindra Sangeet Concert",
+                venue: "Rabindra Sadan",
+                organizer: "Kolkata Music Academy",
+                description: "Celebrate the music of Rabindranath Tagore."
+            }
+        ]
+    },
+    {
+        id: "hyderabad505",
+        title: "hyderabad",
+        cardSet: [
+            {
+                id: 22,
+                image: "https://example.com/hyderabad-event1.jpg",
+                date: "11/22/2023",
+                title: "Biryani Festival",
+                venue: "Charminar Food Street",
+                organizer: "Hyderabad Foodies Club",
+                description: "Indulge in a variety of biryani dishes."
+            },
+            {
+                id: 23,
+                image: "https://example.com/hyderabad-event2.jpg",
+                date: "12/01/2023",
+                title: "Pearl and Jewelry Exhibition",
+                venue: "Nizam's Museum",
+                organizer: "Hyderabad Gem Society",
+                description: "Explore exquisite pearl and jewelry collections."
+            },
+            {
+                id: 24,
+                image: "https://example.com/hyderabad-event3.jpg",
+                date: "12/12/2023",
+                title: "Historical Tour of Golconda Fort",
+                venue: "Golconda Fort",
+                organizer: "Hyderabad Heritage Trust",
+                description: "Discover the history of Golconda Fort."
+            }
+        ]
+    },
+    {
+        id: "jaipur606",
+        title: "jaipur",
+        cardSet: [
+            {
+                id: 25,
+                image: "https://example.com/jaipur-event1.jpg",
+                date: "11/27/2023",
+                title: "Elephant Festival",
+                venue: "Amber Fort",
+                organizer: "Jaipur Cultural Society",
+                description: "Witness the grand Elephant Festival."
+            },
+            {
+                id: 26,
+                image: "https://example.com/jaipur-event2.jpg",
+                date: "12/06/2023",
+                title: "Jaipur Literature Festival",
+                venue: "Diggi Palace",
+                organizer: "Jaipur Literary Society",
+                description: "Engage with renowned authors and poets."
+            },
+            {
+                id: 27,
+                image: "https://example.com/jaipur-event3.jpg",
+                date: "12/16/2023",
+                title: "Rajasthani Folk Music Concert",
+                venue: "Albert Hall Museum",
+                organizer: "Jaipur Music Enthusiasts",
+                description: "Enjoy the rhythms of Rajasthani folk music."
+            }
+        ]
+    },
+    {
+        id: "pune707",
+        title: "pune",
+        cardSet: [
+            {
+                id: 28,
+                image: "https://example.com/pune-event1.jpg",
+                date: "11/24/2023",
+                title: "Pune Food Festival",
+                venue: "Balewadi Stadium",
+                organizer: "Pune Food Lovers Society",
+                description: "A culinary journey with Pune's best food vendors."
+            },
+            {
+                id: 29,
+                image: "https://example.com/pune-event2.jpg",
+                date: "12/03/2023",
+                title: "Classical Music Concert",
+                venue: "Sawai Gandharva Hall",
+                organizer: "Pune Music Academy",
+                description: "An evening of classical music and melodies."
+            },
+            {
+                id: 30,
+                image: "https://example.com/pune-event3.jpg",
+                date: "12/14/2023",
+                title: "Heritage Walk in Pune",
+                venue: "Shaniwar Wada Fort",
+                organizer: "Pune Heritage Foundation",
+                description: "Explore the historical landmarks of Pune."
+            },
+            {
+                id: 31,
+                image: "https://example.com/pune-event4.jpg",
+                date: "11/29/2023",
+                title: "Pune International Film Festival",
+                venue: "INOX Multiplex",
+                organizer: "Pune Film Society",
+                description: "Screenings of international and independent films."
+            },
+            {
+                id: 32,
+                image: "https://example.com/pune-event5.jpg",
+                date: "12/05/2023",
+                title: "Pune Tech Expo",
+                venue: "Aga Khan Palace",
+                organizer: "Pune Tech Innovators",
+                description: "Showcasing the latest in technology and innovation."
+            },
+            {
+                id: 33,
+                image: "https://example.com/pune-event6.jpg",
+                date: "12/18/2023",
+                title: "Pune Yoga and Wellness Retreat",
+                venue: "Osho International Meditation Resort",
+                organizer: "Pune Wellness Retreats",
+                description: "Relax, rejuvenate, and embrace wellness practices."
+            }
         ]
     }
 ]

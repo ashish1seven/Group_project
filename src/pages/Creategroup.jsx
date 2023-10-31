3c56528715036cc17f8069854a7974289abbdb30
Ashish B <cameron.ashish@gmail.com>
2023-10-31 09:28:05 +0530

    final

--- src/pages/Creategroup.jsx
+++ src/pages/Creategroup.jsx
@@ -8,6 +8,7 @@ import offset from "../Images/offset.png";
 import google from "../Images/downloadGoogle.svg";
 import apple from "../Images/downloadApple.svg";
 import Logo from "../Images/Favicon.png";
+import Navbar from "../components/navbar";
 const benefits = [
   {
     id : 1,
@@ -63,82 +64,116 @@ const features = [
 const Creategroup = () => {
   return (
     <>
-      <div className=" h-[85vh] text-white px-52 py-44 text-center" style={{backgroundImage:"url('https://modulescomposer.s3.us-east-2.amazonaws.com/eventpro/header.jpg')",
-      backgroundPosition: "center",
-      backgroundSize: "cover",
-      backgroundAttachment: "fixed",
-    }}>
-        <img src={logo} alt="" className="h-16 mb-10 m-auto" />
-        <h1 className="font-bold text-4xl mb-4">Connect: The App That Links Hearts and Minds</h1>
-        <p className=" text-xl mb-10">
+      <div
+        className=" h-[85vh] text-white px-52 py-44 text-center"
+        style={{
+          backgroundImage:
+            "url('https://modulescomposer.s3.us-east-2.amazonaws.com/eventpro/header.jpg')",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        <img src={logo} alt="" className="h-16 m-auto mb-10" />
+        <h1 className="mb-4 text-4xl font-bold">
+          Connect: The App That Links Hearts and Minds
+        </h1>
+        <p className="mb-10 text-xl ">
           Your New Community Awaits on Connect. Join millions in making real
           connections over shared interests. Start your group today.
         </p>
         <Link
           className="border-2 text-white border-[#FF0079] mr-5 w-[150px] text-center p-2 rounded-md font-bold bg-[#FF0079] hover:bg-[#FF0079]/80"
-          to={"/start"} target='_blank'
+          to={"/start"}
+          target="_blank"
         >
           <button>Start a group</button>
         </Link>
       </div>
       <section className="w-screen mb-10">
-        <h1 className="font-bold text-4xl text-center my-8"><span className="text-[#FF0079]">Connect</span>   makes it easy to build a community</h1>
+        <h1 className="my-8 text-4xl font-bold text-center">
+          <span className="text-[#FF0079]">Connect</span> makes it easy to build
+          a community
+        </h1>
         <div className="flex items-center justify-evenly ">
-        {
-          benefits.map(item=>{
-            return(
-             
-                <div key={item.id} className='flex flex-col items-center w-[220px] h-[200px] justify-evenly'>
-                <img src={item.logo} alt="" className="h-10 rounded-lg"/>
-                <h1 className=" text-xl text-center font-bold ">{item.title}</h1>
-                <p className="text-center text-sm"> {item.description} </p>
-                <Link to={'/start'} target='_blank' className="text-[#FF0079] font-semibold text-sm"><button>START NOW</button></Link>
+          {benefits.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="flex flex-col items-center w-[220px] h-[200px] justify-evenly"
+              >
+                <img src={item.logo} alt="" className="h-10 rounded-lg" />
+                <h1 className="text-xl font-bold text-center ">{item.title}</h1>
+                <p className="text-sm text-center"> {item.description} </p>
+                <Link
+                  to={"/start"}
+                  target="_blank"
+                  className="text-[#FF0079] font-semibold text-sm"
+                >
+                  <button>START NOW</button>
+                </Link>
               </div>
-              
-            )
-          }
-        )}
+            );
+          })}
         </div>
-       
       </section>
       <section className="bg-[#F5DEB3]/80 p-10">
-        <h1 className="font-bold text-4xl text-center my-3">Make 2023 your year with <span className="text-[#FF0079]">Connect!</span>  </h1>
-        <p className="text-center text-sm mb-12">Quickly make new friends with over 60 million users, <br /> all here to connect over shared interests, build professional networks, and have a blast.</p>
-        <div className="flex items-center justify-evenly flex-wrap ">
-          {
-            features.map((item)=>{
-              return(
-                <Link key={item.id}>
-                  <div className="h-[450px] w-[300px] shadow-2xl mb-10 bg-white">
-                  <div className="h-[250px] w-full" style={{backgroundImage:`url(${item.logo})`,backgroundSize:'cover'}}></div>
-                  <div className="flex flex-col items-start justify-evenly p-4">
-                    <h1 className="text-[#FF0079] font-bold text-lg">{item.title}</h1>
+        <h1 className="my-3 text-4xl font-bold text-center">
+          Make 2023 your year with{" "}
+          <span className="text-[#FF0079]">Connect!</span>{" "}
+        </h1>
+        <p className="mb-12 text-sm text-center">
+          Quickly make new friends with over 60 million users, <br /> all here
+          to connect over shared interests, build professional networks, and
+          have a blast.
+        </p>
+        <div className="flex flex-wrap items-center justify-evenly ">
+          {features.map((item) => {
+            return (
+              <Link key={item.id}>
+                <div className="h-[450px] w-[300px] shadow-2xl mb-10 bg-white">
+                  <div
+                    className="h-[250px] w-full"
+                    style={{
+                      backgroundImage: `url(${item.logo})`,
+                      backgroundSize: "cover",
+                    }}
+                  ></div>
+                  <div className="flex flex-col items-start p-4 justify-evenly">
+                    <h1 className="text-[#FF0079] font-bold text-lg">
+                      {item.title}
+                    </h1>
                     <p className="text-sm"> {item.description} </p>
                   </div>
-                  </div>
-                </Link>
-              )
-            })
-          }
+                </div>
+              </Link>
+            );
+          })}
         </div>
       </section>
       <section className="h-[250px] flex justify-evenly p-10">
         <div className="w-[50%]">
-        <h1 className="text-2xl font-bold mb-3 ">Find Your People in 2023.</h1>
-        <p className="text-lg text-left mb-10">Make time for your interests and connect with like-minded individuals. Lasting connections are made on Connect.</p>
-        <Link
-          className="border-2 text-white border-[#FF0079] mr-5 w-[150px] text-center p-2 rounded-md font-bold bg-[#FF0079] hover:bg-[#FF0079]/80"
-          to={"/start"} target='_blank'
-        >
-          <button>Start today</button>
-        </Link>
+          <h1 className="mb-3 text-2xl font-bold ">
+            Find Your People in 2023.
+          </h1>
+          <p className="mb-10 text-lg text-left">
+            Make time for your interests and connect with like-minded
+            individuals. Lasting connections are made on Connect.
+          </p>
+          <Link
+            className="border-2 text-white border-[#FF0079] mr-5 w-[150px] text-center p-2 rounded-md font-bold bg-[#FF0079] hover:bg-[#FF0079]/80"
+            to={"/start"}
+            target="_blank"
+          >
+            <button>Start today</button>
+          </Link>
         </div>
         <div className="hover:scale-110">
           <img src={Logo} alt="" />
         </div>
       </section>
       <footer className="h-[400px] w-full bg-[#1A1831] text-white  px-[170px]">
-      <div className="border-white/40 py-5 flex w-full h-[200px] justify-start mb-5">
+        <div className="border-white/40 py-5 flex w-full h-[200px] justify-start mb-5">
           <div className="mr-56">
             <ul className="">
               <li className="text-lg font-bold">Your Account</li>

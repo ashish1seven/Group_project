3db0d4beb9e60cc1670859b6fc1695cb8c41960e
Ashish B <cameron.ashish@gmail.com>
2023-11-02 17:06:04 +0530

    operators

--- src/components/searchlist.jsx
+++ src/components/searchlist.jsx
@@ -7,18 +7,24 @@ let displayData = searchData;
 
 function Searchlist({ city }) {
   const { darkMode } = useDarkMode();
-  for (let i = 0; i < displayData.length; i++) {
-    
-    if (displayData[i].title === city) {
-      return (
-        <div className={`${darkMode ? "dark" : ""}`}>
-        <div className="flex flex-wrap m-auto w-[1300px] justify-evenly items-center dark:bg-slate-900">
-          <Card data={searchData[i]} />
-          </div>
+  const matchingData = displayData.find(data => data.title.toLowerCase() === city.toLowerCase());
+
+  if (matchingData) {
+    return (
+      <div className={`${darkMode ? "dark" : ""}`}>
+        <div className="flex flex-wrap m-auto w-[1300px] justify-evenly items-center dark:bg-slate-900 mb-5">
+          <Card data={matchingData} />
         </div>
-      );
-    }
+      </div>
+    );
+  } else {
+    return (
+      <div className={`${darkMode ? "dark" : ""}`}>
+        <div className="flex flex-wrap m-auto w-[1300px] justify-center items-center dark:bg-slate-900 mb-10">
+          <h1 className="text-3xl ">No matching events found</h1>
+        </div>
+      </div>
+    );
   }
-  
 }
 export default Searchlist;
